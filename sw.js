fdd9f3ed07da10de03ef2c585bd1a5390c19e540
deanvu <deanvu1@gmail.com>
2025-11-23 21:21:52 +0100

    changees

---
+++ sw.js
@@ -0,0 +1,14 @@
+
+// Minimal Service Worker to satisfy PWA requirements
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple pass-through fetch
+  event.respondWith(fetch(event.request));
+});
